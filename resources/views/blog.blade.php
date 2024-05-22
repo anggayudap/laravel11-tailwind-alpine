390ae82de643b31814ac14f96586bddaa30ed44e
IT <it.angga@dskorea.com>
2024-05-22 13:00:06 +0700

    Eps 6. View Data

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,12 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot>
-    <h3 class="text-xl">Welcome to Blog</h3>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+        {{-- to be continue on https://youtu.be/76YsC4EjGE4 --}}
+        <div>
+            <a href="#">AYP</a> | 1 Januari 2024
+        </div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto illo iste esse labore suscipit distinctio repellendus deleniti laboriosam quis quas. Saepe reprehenderit omnis accusamus architecto odio vero quas ratione ipsa.</p>
+        <a href="#">Read more &raquo;</a>
+    </article>
 </x-layout>
